5b4922039c6bcc53f4137cc62ec90aeacd8a0d8d
Elana Zobin <leka@gnode.org>
2022-04-11 15:41:42 -0700

    checkpoint

--- src/index.js
+++ src/index.js
@@ -12,42 +12,67 @@ function setOutput(run_labels) {
     core.setOutput('run-ons', JSON.stringify(run_labels));
 }
 
-async function start() {
+const startEC2 = async () => {
 
-    const run_labels = {
-    };
+    const ec2_label = config.getEC2RunOnLabel();
+    const userData = await gh.buildUserDataScript(ec2_label);
+    await aws.startRunner(userData);
+    await gh.waitForRunnerRegistered(ec2_label);
+
+    return ec2_label;
+}
+
+const startAzure = async () => {
+
+    const azure_label = config.getAzureRunOnLabel();
+    const userData = await gh.buildUserDataScript(azure_label);
+    await azure.startRunner(userData);
+    await gh.waitForRunnerRegistered(azure_label);
+
+    return azure_label;
+}
+
+
+const start = async() => {
+
+    const run_labels = {ec2: "none", azure: "none"};
+
+    const promises = ["none", "none"];
 
     if (config.input.coulds === 'ec2' || config.input.coulds === 'both') {
         // starting ec2
-        run_labels.ec2 = config.getEC2RunOnLabel();
-        const userData = await gh.buildUserDataScript(run_labels.ec2);
-        await aws.startRunner(userData);
-        await gh.waitForRunnerRegistered(run_labels.ec2);
+        promises[0] = startEC2();
     }
-
     if (config.input.coulds === 'azure' || config.input.coulds === 'both') {
         // starting azure 
-        run_labels.azure = config.getAzureRunOnLabel();
-        const userData = await gh.buildUserDataScript(run_labels.azure);
-        await azure.startRunner(userData);
-        await gh.waitForRunnerRegistered(run_labels.azure);
+        promises[1] = startAzure();
     }
+
+    [run_labels.ec2, run_labels.azure] = await Promise.all(promises);
+
     setOutput(run_labels);
 
 }
 
-async function stop() {
-
+const stopEC2 = async () => {
     const ec2_gh_label = config.getEC2RunOnLabel();
-    const azure_gh_label = config.getAzureRunOnLabel();
-
+    
     core.info(`Removing runner ${ec2_gh_label}`);
     await gh.removeRunner(ec2_gh_label);
-    await aws.stopRunner();
+    await aws.stopRunner()
+}
+
+const stopAzure = async () => {
+    const azure_gh_label = config.getAzureRunOnLabel();
 
     core.info(`Removing runner ${azure_gh_label}`);
     await gh.removeRunner(azure_gh_label);
     await azure.stopRunner();
+}
+
+const stop = async () => {
+
+    await Promise.all([stopEC2(), stopAzure()]);
 }
 
 (async function () {
